00e4d4f675aa006d305a3e69faddd460e3d4ff23
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-03-07 16:18:06 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState, useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
 import './App.css';
 
+function FileInput(props) {
+    const [file, setFile] = props.fileState;
+
+    const onDrop = useCallback(acceptedFile => {
+      setFile(acceptedFile);
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop, accept: '.csv', multiple: false})
+  
+    return (
+      <div className="file-input" {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Read the text below (Please) and drop your CSV file here or <u>click here</u> to select it.</p>
+        }
+      </div>
+    )
+}
+
+function InformationBanner() {
+    return (
+        <div className="information-banner">
+            <div className="text">
+            Create a CSV file from Trading 212 like <a href="https://helpcentre.trading212.com/hc/en-us/articles/360016898917-Can-I-export-the-trading-data-from-my-account-" target="_blank" rel="noreferrer">this</a>. Make sure you export the <i>maximum</i> possible time frame.
+                All uploaded files stay on your PC. You can find the code of this application <a href="#" target="_blank" rel="noreferrer">here</a>.
+            </div>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [file, setFile] = useState(false);
+
+    return (
+        <div className="App">
+            {file ? null : <FileInput fileState={[file, setFile]} />}
+            <InformationBanner />
+        </div>
+    );
 }
 
 export default App;
