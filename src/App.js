7842e3dd5d1c2334a47e9cf3b43dcf7f16ab312e
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-03-07 19:14:18 +0100

    Finish csv uploader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { useState, useCallback } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { Trading212 } from './calculation';
 import './App.css';
 
 function FileInput(props) {
     const [file, setFile] = props.fileState;
 
-    const onDrop = useCallback(acceptedFile => {
-      setFile(acceptedFile);
+    const onDrop = useCallback(acceptedFiles => {
+      setFile(acceptedFiles);
     }, [])
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop, accept: '.csv', multiple: false})
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop, accept: '.csv', multiple: true})
   
     return (
       <div className="file-input" {...getRootProps()}>
@@ -16,7 +17,7 @@ function FileInput(props) {
         {
           isDragActive ?
             <p>Drop the files here ...</p> :
-            <p>Read the text below (Please) and drop your CSV file here or <u>click here</u> to select it.</p>
+            <p>Read the text below (i beg you) and drop your CSV file here or <u>click here</u> to select it.</p>
         }
       </div>
     )
@@ -26,19 +27,97 @@ function InformationBanner() {
     return (
         <div className="information-banner">
             <div className="text">
-            Create a CSV file from Trading 212 like <a href="https://helpcentre.trading212.com/hc/en-us/articles/360016898917-Can-I-export-the-trading-data-from-my-account-" target="_blank" rel="noreferrer">this</a>. Make sure you export the <i>maximum</i> possible time frame.
-                All uploaded files stay on your PC. You can find the code of this application <a href="#" target="_blank" rel="noreferrer">here</a>.
+            Create a CSV file from Trading 212 like <a href="https://helpcentre.trading212.com/hc/en-us/articles/360016898917-Can-I-export-the-trading-data-from-my-account-" target="_blank" rel="noreferrer">this</a>. Export every possible year, you will get one CSV per year.
+                All uploaded files stay on your PC. You can find the code of this application or report issues <a href="https://github.com/martenmatrix/trading212-tax-generator" target="_blank" rel="noreferrer">here</a>.
             </div>
         </div>
     )
 }
 
+function LoadingAnimation() {
+    return (
+        <></>
+    )
+}
+
+function Report(props) {
+    const cbWhenReady = props.cbWhenReady;
+    const files = props.files;
+
+    function csvToArray(str, delimiter = ",") {
+        // slice from start of text to the first \n index
+        // use split to create an array from string by delimiter
+        const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+      
+        // slice from \n index + 1 to the end of the text
+        // use split to create an array of each csv value row
+        const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+      
+        // Map the rows
+        // split values from each row into an array
+        // use headers.reduce to create an object
+        // object properties derived from headers:values
+        // the object passed as an element of the array
+        const arr = rows.map(function (row) {
+          const values = row.split(delimiter);
+          const el = headers.reduce(function (object, header, index) {
+            object[header] = values[index];
+            return object;
+          }, {});
+          return el;
+        });
+      
+        // return the array
+        return arr;
+    }
+      
+    async function getFilesAsText(files) {
+        let filesText = '';
+
+        const readUploadedFileAsText = (inputFile) => {
+            const temporaryFileReader = new FileReader();
+          
+            return new Promise((resolve, reject) => {
+              temporaryFileReader.onerror = () => {
+                temporaryFileReader.abort();
+                reject(new DOMException("Problem parsing input file."));
+              };
+          
+              temporaryFileReader.onload = () => {
+                resolve(temporaryFileReader.result);
+              };
+              temporaryFileReader.readAsText(inputFile);
+            });
+        };
+
+        await Promise.all(files.map(async (file) => {
+            const text = await readUploadedFileAsText(file);
+            filesText += text;
+        }));
+
+        return filesText;
+    }
+
+    async function getArray() {
+        const text = await getFilesAsText(files);
+        const array = csvToArray(text);
+        console.log(array);
+    }
+
+    useEffect(() => {
+        getArray();
+
+    }, []);
+
+    return null;
+}
+
 function App() {
-    const [file, setFile] = useState(false);
+    const [files, setFiles] = useState(false);
 
     return (
         <div className="App">
-            {file ? null : <FileInput fileState={[file, setFile]} />}
+            {files ? <Report files={files} /> : <FileInput fileState={[files, setFiles]} />}
             <InformationBanner />
         </div>
     );
