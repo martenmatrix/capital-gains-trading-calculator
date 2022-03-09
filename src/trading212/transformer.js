e6de4ad7a2285c7cdeb716e72459026a1a5a5540
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-03-09 22:00:07 +0100

    Refactor files

---
+++ src/trading212/transformer.js
@@ -0,0 +1,128 @@
+// Converts a complete ISO date string in UTC time, the typical format for transmitting a date in JSON, to a JavaScript Date instance.
+import { format } from 'date-fns';
+import { getObjectsSortedByDate } from '../misc';
+
+const Trading212 = (function () {
+    const actionsDone = [];
+
+    function addActions(actions) {
+        actionsDone.push(...actions);
+    }
+
+    function getActionsWithoutDuplicates(actions) {
+        // creates array of stringified object and normal object of every current object in array
+        const stringifiedAndNormal = actions.map(element => [JSON.stringify(element), element]);
+        const withoutDuplicates = new Map(stringifiedAndNormal);
+        const withoutStringifiedObjects = withoutDuplicates.values();
+        // convert iterator to array
+        const actionsWithoutDuplicates = [...withoutStringifiedObjects];
+        return actionsWithoutDuplicates;
+    }
+
+    function getConvertedActions() {
+        const withoutDuplicates = getActionsWithoutDuplicates(actionsDone);
+        const sortedByDate = getObjectsSortedByDate(withoutDuplicates, 'Time');
+        return sortedByDate;
+    }
+
+    function getTotalCurrencyKey(actions) {
+        const keys = Object.keys(actions[0]);
+        const regex = /Total \([A-Z]{3}\)/;
+        for (const key of keys) {
+            if (regex.test(key)) {
+                return key;
+            }
+        }
+    }
+
+    function getConversionFeeKey(actions) {
+        const keys = Object.keys(actions[0]);
+        const regex = /Currency conversion fee \([A-Z]{3}\)/;
+        for (const key of keys) {
+            if (regex.test(key)) {
+                return key;
+            }
+        }
+    }
+
+    function getSpecificActions(actions, onlyAction) {
+        return actions.filter(action => action.Action === onlyAction);
+    }
+
+    // deducts fx fees from total price
+    function removeCurrencyConversionFeesFromTotal(actions) {
+        const currencyKey = getTotalCurrencyKey(actions);
+        const conversionKey = getConversionFeeKey(actions);
+
+        const removedFees = actions.map(action => {
+            const newObject = {...action};
+            const fee = parseFloat(action[conversionKey]);
+            const total = parseFloat(action[currencyKey]);
+            if (!(isNaN(fee))) {
+                const totalWithoutFee = total - fee;
+                newObject[currencyKey] = toString(totalWithoutFee);
+                newObject['FeesDeductedFromTotal'] = true;
+            }
+            return newObject;
+        });
+        return removedFees;
+    }
+
+    function getRealizedGains(actions, method, years) {
+        if (method === 'fifo') {
+        }
+    }
+    
+    function getPossibleYears() {
+        const actions = getConvertedActions();
+    
+        const possibleYears = [];
+        actions.forEach(action => {
+            const date = action.Time;
+            const year = format(date, 'yyyy');
+        
+            if (!possibleYears.includes(year)) {
+                possibleYears.push(year);
+            }
+        });
+        
+        return possibleYears;
+    }
+
+    function convertForFIFOCalculation(actions) {
+        const convertedActions = [];
+        actions.map(action => {
+            const convertedAction = {};
+            convertedAction.amount = parseFloat(action['No. of shares']);
+            convertedAction.date = action.Time;
+            convertedAction.totalPrice = parseFloat(action[getTotalCurrencyKey(actions)]);
+            convertedAction.symbol = action['Currency (Price / share)'];
+            const getAction = () => {
+                if (action.Action === 'Market buy') return 'BUY';
+                else if (action.Action === 'Market sell') return 'SELL';
+                else return action.Action;
+            }
+            convertedAction.type = getAction();
+
+            return convertedAction;
+        });
+    }
+
+    function getRealizedProfits(method) {
+        const actions = getConvertedActions();
+        const actionsWithoutFees = removeCurrencyConversionFeesFromTotal(actions);
+        const onlyBuys = getSpecificActions(actionsWithoutFees, 'Market buy');
+        const onlySells = getSpecificActions(actionsWithoutFees, 'Market sell');
+        const onlySellsAndBuys = [...onlyBuys, ...onlySells];
+        const FIFOFormat = convertForFIFOCalculation(onlySellsAndBuys);
+        console.log(FIFOFormat);
+    }
+
+    function getCurrencyExchangeFees() {
+
+    }
+
+    return { addActions, getRealizedProfits, getCurrencyExchangeFees, getPossibleYears };
+})()
+
+export default Trading212;
