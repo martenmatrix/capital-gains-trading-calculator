047361bf1712e06f096da4ae68944e264fd624da
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-03-11 12:09:02 +0100

    Add donation banner and bootstrap

--- src/trading212/Report.js
+++ src/trading212/Report.js
@@ -1,10 +1,36 @@
 import { useEffect, useState } from 'react';
 import Trading212 from './transformer';
 import LoadingAnimation from '../LoadingAnimation';
+import Button from 'react-bootstrap/Button';
+
+function DonationBanner(props) {
+    const showAfter = props.showAfter;
+    const [isShown, setIsShown] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsShown(true);
+        }, showAfter);
+    }, [showAfter]);
+
+    if (isShown) return (
+        <div className="donation-banner">
+            <div className="text">
+                If this tool saved you some time, consider making a small donation today, because maintaining and hosting this site creates costs. Every penny helps and 100% of your donation immediately goes into this project.
+            </div>
+            <Button size="lg" variant="primary" href="https://www.paypal.com/donate/?hosted_button_id=HSPL5HCL7A6P6">Donate now!</Button>
+        </div>
+    );
+    else return null;
+}
 
 function Statistics(props) {
     const year = props.year;
-    const profit = props.profit;
+    const fifoData = props.fifoData;
+    const round = (number) => Math.round(number * 1000) / 1000;
+    const profit = round(fifoData.total);
+    const income = round(fifoData.income);
+    const loss = round(fifoData.loss);
     const conversionFees = props.conversionFees;
     const currency = props.currency;
 
@@ -15,12 +41,16 @@ function Statistics(props) {
             <div className="information">
                 The following results were calculated with the FiFo method.
             </div>
+            <div className="profits">
+                You made a {madeLoss ? 'loss' : 'profit'} of {`${Math.abs(profit)} ${currency}`} (fees excluded) in {year}. {madeLoss ? "I'm sorry. " : "Good job! "}
+                Your profit consist of a loss of {`${Math.abs(loss)} ${currency}`} and an income of {`${Math.abs(income)} ${currency}`}.
+            </div>
             <div className="fees">
                 You paid a total of {`${conversionFees} ${currency}`} for conversion fees.
             </div>
-            <div className="profits">
-                You made a {madeLoss ? 'loss' : 'profit'} of {`${Math.abs(profit)} ${currency}`} (fees excluded) in {year}. {madeLoss ? "I'm sorry." : "Good job!"}
-            </div>
+            <pre>
+                <code>{JSON.stringify(fifoData, null, 2)}</code>
+            </pre>
         </div>
     )
 }
@@ -33,7 +63,8 @@ function Trading212Report(props) {
     const [conversionFees, setConversionFees] = useState([]);
     const [currency, setCurrency] = useState();
 
-    const [, setFifo] = useState();
+    const [fifo, setFifo] = useState();
+    const [fifoData, setFifoData] = useState();
 
     const [currentTask, setCurrentTask] = useState(null);
 
@@ -57,15 +88,18 @@ function Trading212Report(props) {
     }, [csv])
 
     useEffect(() => {
+        if (!fifo) return;
+
         setCurrentTask('Getting fees');
         const conversionFees = Trading212.getCurrencyConversionFees(selectedYear);
         setConversionFees(conversionFees);
 
         setCurrentTask('Getting profits');
-        
+        const data = fifo.getRealizedProfits(selectedYear);
+        setFifoData(data)
 
         setCurrentTask(null);
-    }, [selectedYear]);
+    }, [fifo, selectedYear]);
 
     function handleChange(e) {
         const selectedYear = e.target.value;
@@ -82,12 +116,8 @@ function Trading212Report(props) {
 
                 {selectedYear ? <Statistics year={selectedYear}
                                             conversionFees={conversionFees}
-                                            profit={20}
+                                            fifoData={fifoData}
                                             currency={currency}/> : null}
-
-                <div className="donation">
-                    {/* only show how results were calculated */}
-                </div>
             </div>
         )
     } else {
