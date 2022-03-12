d5fdf61f724f9cc40a33a668f5dce304d5ca18aa
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-03-12 10:33:24 +0100

    No extra folders for different assets Revolut

---
+++ src/revolut/transformer.js
@@ -0,0 +1,10 @@
+const Revolut = (function () {
+    const actionsDone = [];
+    
+    function addActions(actions) {
+        actionsDone.push(...actions);
+    }
+
+})();
+
+export default Revolut;
---
+++ src/tests/revolut/transformer.test.js
@@ -0,0 +1,1 @@
+import transformer from '../../revolut/goldAndSilver/report.js'
