7c19093d9296dd507cb48bf18e5f4118d3d1e688
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-03-13 17:15:52 +0100

    Add displaying of results for revolut

--- src/revolut/Report.js
+++ src/revolut/Report.js
@@ -6,7 +6,11 @@ function RevolutReport(props) {
     const csvArray = props.csv;
     const [task, setTask] = useState(null);
     const [years, setYears] = useState([]);
+    const [selectedYear, setSelectedYear] = useState();
     const [currency, setCurrency] = useState();
+
+    const [fees, setFees] = useState();
+    const [FiFoData, setFiFoData] = useState();
 
     useEffect(() => {
         setTask('Adding trading history')
@@ -23,14 +27,28 @@ function RevolutReport(props) {
         setTask(null);
     }, [csvArray]);
 
-    function handleYearSelection(year) {
+    useEffect(() => {
+        setTask('Getting fees');
+        const fees = Revolut.getFees(selectedYear);
+        setFees(fees);
 
+        setTask('Calculating FiFo data');
+        const FiFo = Revolut.getFiFo();
+        const FiFoData = FiFo.getRealizedProfits(selectedYear);
+        setFiFoData(FiFoData);
+
+        setTask(null);
+    }, [selectedYear]);
+
+    function handleYearSelection(year) {
+        setSelectedYear(year);
     }
 
     return <FiFoReport 
         handleYearSelection={handleYearSelection}
         currentTask={task}
         years={years}
+        selectedYear={selectedYear}
         currency={currency}
     />;
 };
