479ab440b6c34eac6d5cf9d5fbdf0b0499c99f69
Marten <derapfelesser@aol.de>
2022-06-16 00:19:20 +0200

    Assert valid date in test

--- src/tests/misc.test.js
+++ src/tests/misc.test.js
@@ -5,5 +5,5 @@ test('able to convert dates in the following format dd/mm/yyyy hh:mm', () => {
    
    const objectsWithConvertedDates = convertDateStringsToDateObjects(objectWithDate, 'date');
 
-   expect(objectsWithConvertedDates).toEqual([{ date: new Date('25/03/2003 15:16') }, { date: new Date('20/03/2003 15:16') }]);
+   expect(objectsWithConvertedDates).toEqual([{ date: new Date(2003, 3, 25, 15, 16) }, { date: new Date(2003, 3, 20, 15, 16) }]);
 });
