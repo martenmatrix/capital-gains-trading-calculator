98f88a216a53b2970e9773fd71791b163b6f1183
Marten <derapfelesser@aol.de>
2022-06-16 00:48:51 +0200

    Revert "Create failing tes with new date format"
    This reverts commit 48e26592a29fb1c85c3daf03d2ccee89093e5bb5.

--- src/tests/misc.test.js
+++
@@ -1,9 +0,0 @@
-import { convertDateStringsToDateObjects } from "../misc";
-
-test('able to convert dates in the following format dd/mm/yyyy hh:mm', () => {
-   const objectWithDate = [{ date: '25/03/2003 15:16' }, { date: '20/03/2003 15:16' }];
-   
-   const objectsWithConvertedDates = convertDateStringsToDateObjects(objectWithDate, 'date');
-
-   expect(objectsWithConvertedDates).toEqual([{ date: new Date(2003, 3, 25, 15, 16) }, { date: new Date(2003, 3, 20, 15, 16) }]);
-});
